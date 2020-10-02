0ad9559e0baa288cb6b8f6550d7baf2f703c6ef5
Ragini Mangla <raginimangla@gmail.com>
2020-10-02 15:34:26 -0400

    Practice Question 1: Write function to greet in diff gender. If input is capital M or small m.. output is "hello sir" If it's F or f.. output is "hello miss" . If input is anything else.... output is "hello".

--- ragini1.py
+++ ragini1.py
@@ -1,1 +1,12 @@
-print("hello aditya")
+def greeting(gender):
+    if gender == "Male" or gender == "M":
+        return "Hello Sir!"
+    elif gender == "Female" or gender == "F":
+        return "Hello Miss!"
+    else:
+        return "Hello there!"
+
+print(greeting("Male"))
+print(greeting("M"))
+print(greeting("Female"))
+print(greeting("F"))
