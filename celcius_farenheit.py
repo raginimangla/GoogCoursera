f13aad2991646b4810bb39d7c203188232bd9648
Ragini Mangla <raginimangla@gmail.com>
2020-10-05 17:20:18 -0400

    Write a func to convert celcius to Farenheit formula F = C* 1.8 + 32

--- celcius_farenheit.py
+++ celcius_farenheit.py
@@ -2,10 +2,8 @@ def cel_fht(celtemp): #celtemp = temperature entered in degree celcius
     if celtemp <= -273:
         return "Invalid input"
 
-    elif celtemp > -273:
+    else:
         return ((celtemp * 1.8) + 32)
-
-
 
 print(cel_fht(25))
 print((cel_fht(-273)))
